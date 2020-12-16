fe79c509065063e2c24f88c932d0441aaa56b6b8
chaitra <72888497+Chaitra001-smartcoder@users.noreply.github.com>
2020-12-17 01:58:24 +0530

    Updates

---
+++ precache-manifest.17d05680ac9077878cf02a95740f7216.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f903a6f51a9e9adb94ca241057752260",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "159877ed18b858ee2ab7",
+    "url": "/covidtracker/static/css/main.8b4aa5eb.chunk.css"
+  },
+  {
+    "revision": "909e15b53aba5219af46",
+    "url": "/covidtracker/static/js/2.e823ae2d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.e823ae2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159877ed18b858ee2ab7",
+    "url": "/covidtracker/static/js/main.38fff122.chunk.js"
+  },
+  {
+    "revision": "49d87951868f3c9023e8",
+    "url": "/covidtracker/static/js/runtime-main.1440f5f1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/image.d7265326.png"
+  }
+]);
